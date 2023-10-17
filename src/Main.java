0ce1ad8e7362a81896f79251d544a3a26d0328fc
Samiir Ahmed <samiir.mahamed6@gmail.com>
2023-10-17 10:04:28 +0200

    first comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!"); // här skriver vi ut något på skärmen
+        // hej hej där welcome hej
 
 
     }
